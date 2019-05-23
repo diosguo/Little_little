9112e265eebd7a4be3d1e2e7104b44a6c9190b4f
theDoctor2013 <943024256@qq.com>
2019-05-23 13:59:39 +0800

    complete youdao md convert

--- 有道云笔记Markdown转为CSDNMarkdown/youdaoNoteMD2MD.py
+++ 有道云笔记Markdown转为CSDNMarkdown/youdaoNoteMD2MD.py
@@ -1,1 +1,28 @@
-print('hello')
+import re
+import sys
+
+def main(file_to_process, file_to_save):
+    fin = open(file_to_process,'r')
+    fou = open(file_to_save,'w')
+    math_block = False
+    for line in fin:
+        line = line.replace('`$','$')
+        line = line.replace('$`','$')
+        if '```math' in line:
+            line = line.replace('```math','$$')
+            math_block = True
+        if '```' in line and math_block is True:
+            print(line)
+            math_block = False
+            line = line.replace('```','$$')
+        fou.write(line)
+
+    fin.close()
+    fou.close()
+
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
